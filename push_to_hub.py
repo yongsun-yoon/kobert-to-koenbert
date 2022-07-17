2a936640f87373a2d19827e360fe9804524e07d5
Yoon <respect5716@gmail.com>
2022-07-17 08:02:07 +0000

    Update push_to_hub

--- push_to_hub.py
+++ push_to_hub.py
@@ -1,13 +1,20 @@
+import shutil
 from transformers import AutoTokenizer, AutoModel
 
-def main():
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--repo_path_or_name', type=str, default='respect5716/koenbert-dev')
+args = parser.parse_args()
+
+def main(args):
     tokenizer = AutoTokenizer.from_pretrained('koen_bert_tuned')
     model = AutoModel.from_pretrained('koen_bert_tuned')
 
-    tokenizer.push_to_hub('koenbert-base')
-    model.push_to_hub('koenbert-base')
+    tokenizer.push_to_hub(args.repo_path_or_name)
+    model.push_to_hub(args.repo_path_or_name)
+    shutil.rmtree(args.repo_path_or_name)
     print('push to hub 완료')
 
 
 if __name__ == '__main__':
-    main()
+    main(args)
